8fd941bfd979b0e3eadc54b2c031c25fdbe00069
Lechiana <tayneedanime@gmail.com>
2021-08-24 15:23:52 +0700

    Initial commit

---
+++ src/data/tattoos.js
@@ -0,0 +1,10 @@
+const tattoos = [
+    { title: "เคียน่า", thumbnailUrl: "/images/tattoo-01-small.jpg", fullUrl: "/images/tattoo-01-large.jpg" },
+    { title: "เมย์", thumbnailUrl: "/images/tattoo-02-small.jpg", fullUrl: "/images/tattoo-02-large.jpg" },
+    { title: "ยาเอะ", thumbnailUrl: "/images/tattoo-03-small.jpg", fullUrl: "/images/tattoo-03-large.jpg" },
+    { title: "ฮิเมโกะ", thumbnailUrl: "/images/tattoo-04-small.jpg", fullUrl: "/images/tattoo-04-large.jpg" },
+    { title: "เบียงก้า", thumbnailUrl: "/images/tattoo-05-small.jpg", fullUrl: "/images/tattoo-05-large.jpg" },
+    { title: "ฟูกะ", thumbnailUrl: "/images/tattoo-06-small.jpg", fullUrl: "/images/tattoo-06-large.jpg" },
+];
+
+export default tattoos;
